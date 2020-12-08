811840e5c236cf03cc3e10b813daf4c2bd629124
James Ferrell <jamesferrell@Jamess-MacBook-Pro.local>
2020-12-07 22:54:22 -0500

    server change; working skel

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const PORT = process.env.NODE_ENV || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
